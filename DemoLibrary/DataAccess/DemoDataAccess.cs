965c8f7e3076070df777362a21bf365f78876ba4
Lakmina Egodawattha <lakmina@bistecglobal.com>
2023-07-09 22:32:03 +0530

    2nd commit

--- DemoLibrary/DataAccess/DemoDataAccess.cs
+++ DemoLibrary/DataAccess/DemoDataAccess.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DemoLibrary.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,25 @@ using System.Threading.Tasks;
 
 namespace DemoLibrary.DataAccess
 {
-    public class DemoDataAccess
+    public class DemoDataAccess : IDataAccess
     {
+        private List<PersonModel> people = new();
+
+        public DemoDataAccess()
+        {
+            people.Add(new PersonModel() { Id = 1, FirstName = "Tim", LastName = "Corey" });
+            people.Add(new PersonModel() { Id = 2, FirstName = "kanishka", LastName = "Lakmina" });
+        }
+        public List<PersonModel> GetPeople()
+        {
+            return people;
+        }
+        public PersonModel InsertPerson(string firstName, string lastName)
+        {
+            PersonModel p = new() { FirstName = firstName, LastName = lastName };
+            p.Id = people.Max(x => x.Id) + 1;
+            people.Add(p);
+            return p;
+        }
     }
 }
--- DemoLibrary/Models/PersonModel.cs
+++ DemoLibrary/Models/PersonModel.cs
@@ -6,7 +6,10 @@ using System.Threading.Tasks;
 
 namespace DemoLibrary.Models
 {
-    internal class PersonModel
+    public class PersonModel
     {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
     }
 }
